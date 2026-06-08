16053045122ab7b893b5f53ebbafa2e9327afdda
aadicoder1 <aadityasahu1203@gmail.com>
2026-06-08 19:48:18 +0530

    week2 day1 done

---
+++ week2/day1/RansomNote.java
@@ -0,0 +1,17 @@
+package week2.day1;
+
+class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
+        for (char ch:magazine.toCharArray()) {
+            freq[ch-'a']++;
+        }
+        for (char ch:ransomNote.toCharArray()){
+            freq[ch-'a']--;
+            if(freq[ch-'a']<0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
