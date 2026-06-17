c28bc0704c5068f99e96d2d9635680b15d49af47
aadicoder1 <aadityasahu1203@gmail.com>
2026-06-17 14:07:22 +0530

    week3 day3 done

---
+++ week3/day3/ValidParentheses.java
@@ -0,0 +1,30 @@
+package week3.day3;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='('||ch=='[' || ch=='{'){
+                stack.push(ch);
+            }
+            else {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top=stack.pop();
+                if(ch==')' && top!='('){
+                    return false;
+                }
+                if(ch==']' && top!='['){
+                    return false;
+                }
+                if(ch=='}' && top!='{'){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
