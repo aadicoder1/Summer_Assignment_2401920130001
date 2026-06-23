45c8e1798dc7752ca8a24167e89054660890fdeb
aadicoder1 <aadityasahu1203@gmail.com>
2026-06-23 15:32:17 +0530

    week4 day2 done

---
+++ week4/day2/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,39 @@
+package week4.day2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeZigzagLevelOrderTraversal {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result=new ArrayList<>();
+        if(root==null){
+            return result;
+        }
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        boolean leftToRight=true;
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            List<Integer> level=new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode node=queue.poll();
+                level.add(node.val);
+                if(node.left!=null) {
+                    queue.offer(node.left);
+                }
+                if(node.right!=null) {
+                    queue.offer(node.right);
+                }
+            }
+            if(!leftToRight){
+                Collections.reverse(level);
+            }
+            result.add(level);
+            leftToRight=!leftToRight;
+        }
+        return result;
+    }
+}
