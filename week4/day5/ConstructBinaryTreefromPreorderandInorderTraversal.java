ca4b0b0708b444768c201b4adba26a9cbdac532a
aadicoder1 <aadityasahu1203@gmail.com>
2026-06-26 10:07:00 +0530

    week4 day5 done

---
+++ week4/day5/ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -0,0 +1,26 @@
+package week4.day5;
+
+import java.util.HashMap;
+
+public class ConstructBinaryTreefromPreorderandInorderTraversal {
+    int preIndex=0;
+    HashMap<Integer,Integer> map=new HashMap<>();
+    public TreeNode buildTree(int[] preorder,int[] inorder){
+        for(int i=0;i<inorder.length;i++){
+            map.put(inorder[i],i);
+        }
+        return build(preorder,0,inorder.length-1);
+    }
+
+    private TreeNode build(int[] preorder,int left,int right){
+        if(left>right){
+            return null;
+        }
+        int rootValue = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootValue);
+        int mid = map.get(rootValue);
+        root.left = build(preorder, left, mid-1);
+        root.right = build(preorder, mid+1, right);
+        return root;
+    }
+}
